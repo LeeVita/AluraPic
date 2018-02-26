9ba87cdf921e5696d37a6bef4901af5f0984e650
Vita Lee <vita-lee@msn.com>
2018-02-26 09:56:50 -0300

    Added photo component

---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, Input } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: 'foto',
+    templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+    @Input() titulo;
+    @Input() url;
+}
